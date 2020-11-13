aa749a9ba150aa2471289b954d9b54845e3bba34
Elias salazar <eliassalazar543@gmail.com>
2020-11-13 16:50:32 -0600

    version 1.0

--- Lista.java
+++ Lista.java
@@ -1,8 +1,15 @@
+import javax.swing.JOptionPane;
+
 /**
  * @file Lista.java
+ * @author Elias Salazar Reyes
+ * @version 0.0.1
+ * @brief listas basicas
  */
 public class Lista{
-
+ /**
+  * 
+  */
     private int tamaño;
     private Nodo inicio;
 
@@ -21,7 +28,7 @@ public class Lista{
 
     public void push(int valor) {
         Nodo n = new Nodo();
-        n. setValor(valor);
+        n.setValor(valor);
 
         if (vacio()) {
             inicio=n;
@@ -35,6 +42,75 @@ public class Lista{
       tamaño++;  
     }
 
+    public void listar(){
+        if (!vacio()) {
+            Nodo aux = inicio;
+            int i=1;
+            String aux2 = " ";
+            while (aux !=null) {
+                aux2 +="pos:"+i+"["+aux.getValor()+"]\n";
+                aux=aux.getSiguiente();
+                i++;
+                }
+                JOptionPane.showMessageDialog(null, aux2);
+            } else{
+            JOptionPane.showMessageDialog(null, "null");
+        }
 
-    
+    }
+
+    public void agregarInicio(int valor){
+        Nodo n = new Nodo();
+        n.setValor(valor);
+        if (vacio()) {
+            inicio=n;
+        }else{
+            n.setSiguiente(inicio);
+            inicio=n;
+        }
+        tamaño++;
+    }
+
+    public boolean buscar(int numero) {
+        Nodo aux = inicio;
+        
+        boolean encontrado=false;
+        while (aux !=null && encontrado != true) {
+            if(numero==aux.getValor()){
+                encontrado=true;
+            } else{
+                aux=aux.getSiguiente();
+            }
+        }
+        return encontrado;
+    }
+
+    public void modificar(int numero,int valor) {
+        if (buscar(numero)) {
+            Nodo aux=inicio;
+            while (aux.getValor() != numero){
+                aux=aux.getSiguiente();
+            }
+            aux.setValor(valor);
+        }
+    }
+
+    public void eliminar(int pos) {
+        pos-=1;
+        if (pos >=0 && pos <=tamaño) {
+            if (pos==0) {
+                inicio=inicio.getSiguiente();
+            } else{
+                Nodo aux=inicio;
+                for (int i=0; i<pos-1;i++){
+                    aux=aux.getSiguiente();
+                }
+                Nodo siguiente = aux.getSiguiente();
+                aux.setSiguiente(siguiente.getSiguiente());
+            }
+            tamaño--;
+        } else{
+            JOptionPane.showMessageDialog(null,"La lsita no contiene ese dato");
+        }
+    }
 }
--- Principal.java
+++ Principal.java
@@ -1,31 +1,66 @@
-import javax.swing.*;
+import javax.swing.JOptionPane;
 public class Principal {
     public static void main(String[] args) throws Exception {
         Lista l = new Lista();
         int aux=0;
-        boolean vacio=l.vacio();
         do{
-        int op=Integer.parseInt(JOptionPane.showInputDialog(null, "1.Agregar valor\n2.Tamaño de lista\n3.Es vacia\n"));
+        int op=Integer.parseInt(JOptionPane.showInputDialog(null, "1.Agregar valor\n2.Tamaño de lista\n3. Comprobar si esta vacia"+
+        "\n4.Listar\n5.Agregar al inicio\n"+
+        "6.Buscar\n7.Modificar\n8.Eliminar\n9.Salir"));
         switch (op) {
             case 1:
                 int valor=Integer.parseInt(JOptionPane.showInputDialog(null, "Valor a agregar:"));
                 l.push(valor);
+                aux=1;
                 break;
             case 2:
-                JOptionPane.showMessageDialog(null, "contiene "+l.getTamaño()+"elementos.");
+                JOptionPane.showMessageDialog(null, "contiene "+l.getTamaño()+" elementos.");
+                aux=1;
             break;
 
             case 3:
-                if (vacio) {
+                if (l.vacio()) {
                     JOptionPane.showMessageDialog(null, "La lista esta vacia");
                 } else{
                     JOptionPane.showMessageDialog(null, "La lista no esta vacia");
                 }
+                aux=1;
             break;
-
+            case 4:
+                l.listar();
+                aux=1;
+            break;
+            case 5:
+                int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "Valor a agregar al inicio"));
+                l.agregarInicio(numero);
+                aux=1;
+            break;
+            case 6:
+                numero = Integer.parseInt(JOptionPane.showInputDialog(null,"Valor a buscar"));
+                if (l.buscar(numero)==true) {
+                    JOptionPane.showMessageDialog(null,"El numero se encuentra en la lista");
+                } else{
+                    JOptionPane.showMessageDialog(null,"El numero no se encuentra en la lista");
+                }
+                aux=1;
+            break;
+            case 7:
+                numero = Integer.parseInt(JOptionPane.showInputDialog(null,"Valor a modificar"));
+                valor =Integer.parseInt(JOptionPane.showInputDialog(null,"Nuevo valor"));
+                l.modificar(numero, valor); 
+                aux=1;   
+            break;
+            case 8:
+                int pos=Integer.parseInt(JOptionPane.showInputDialog(null,"Posicion a eliminar"));
+                l.eliminar(pos);
+                aux=1;
+            break;
+            case 9:
+            aux=0;
+            break;
             default:
+                JOptionPane.showMessageDialog(null,"opcion no validad");
             }
-             aux = Integer.parseInt(JOptionPane.showInputDialog(null, "Quiere hacer otra operacion? 1=si/2=no"));
         } while(aux==1);
     }
 }
